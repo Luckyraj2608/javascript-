c679e732fea1fe686fb1eba42508081928f8a07e
unknown <coollucky2608@gmail.com>
2025-02-22 23:58:40 +0530

    Eligible JS Code

---
+++ eligible.js
@@ -0,0 +1,15 @@
+let age = 18
+if(age < 18)
+{
+    console.log("You are not eligible.");
+
+}
+else if(age >= 18 && age <=40)
+{
+    console.log("You are eligible");
+    
+}
+else{
+    console.log("You are Old");
+    
+}

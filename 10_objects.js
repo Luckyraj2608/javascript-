15e99d9a014d1f8725d30b6308830bb29e8f952d
unknown <coollucky2608@gmail.com>
2025-02-28 00:06:29 +0530

    Object_02 in JS

--- 10_objects.js
+++ 10_objects.js
@@ -4,11 +4,16 @@
 
 // Object.create
 
+const mySym = Symbol("key1")
+
 const JsUser = {
     name: "Lucky",
+    [mySym]: "mykey1",
     age: 18,
     location: "Bihar",
     email: "coollucky2608@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["monday", "saturday"]
       
 } 
 
@@ -18,5 +23,28 @@ console.log(abc);
 console.log(JsUser); 
 console.log(JsUser.name);
 console.log(JsUser.email);
+console.log(JsUser["email"]);
+console.log(JsUser[mySym]);
+
+JsUser.email = "lucky2608@gmail.com"
+Object.freeze(JsUser)
+JsUser.email = "luckygoogle.com"
+console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+    
+}
+JsUser.greetingTWo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+    
+}
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTWo());
+
+
+
+
+
 
 

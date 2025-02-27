058c6430194e57998862c7e5ea08aa82899f3d15
unknown <coollucky2608@gmail.com>
2025-02-27 23:32:59 +0530

    Object in JS

---
+++ 10_objects.js
@@ -0,0 +1,22 @@
+// singleton
+
+// object literals
+
+// Object.create
+
+const JsUser = {
+    name: "Lucky",
+    age: 18,
+    location: "Bihar",
+    email: "coollucky2608@gmail.com",
+      
+} 
+
+const abc = JsUser.name
+console.log(abc);
+
+console.log(JsUser); 
+console.log(JsUser.name);
+console.log(JsUser.email);
+
+

a0372ba52eb7037ff2ab0bd8c26c94fcbb741807
unknown <coollucky2608@gmail.com>
2025-03-01 21:13:29 +0530

    _JSON in JS

---
+++ 12_JSON.js
@@ -0,0 +1,32 @@
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "Rahwar",
+}
+
+// course.courseInstructor
+
+const {courseInstructor : instructor} = course
+
+console.log(instructor);
+
+// const navbar = ({}) => {
+
+// }   deconstruct kiya jaata hai
+
+// navbar(company = "Rahwar")
+
+// {
+//     "name": "rahwar",
+//     "coursename": "js in hindi",
+//     "price": "free",
+// }
+
+[
+    {},
+    {},
+    {},
+]
+
+
+

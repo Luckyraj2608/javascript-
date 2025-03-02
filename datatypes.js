4896bb0c92088b2871c3b8fbb86bc99a894a4d65
unknown <coollucky2608@gmail.com>
2025-03-02 23:26:30 +0530

    practice_02

--- datatypes.js
+++ datatypes.js
@@ -27,3 +27,4 @@ console.log(typeof undefined);
 
 
 
+
---
+++ practice_02.js
@@ -0,0 +1,26 @@
+//  ...--REst k wajah se sbb array me aa gya hai
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200, 400, 500, 2000));
+
+
+const user = {
+    username: "handleObject",
+    price: 199,
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price} `);
+    
+}
+
+// handleObject(user)
+handleObject({
+    username : "sam",
+    price: 399,
+})
+
+
+const myNewArray = [200, 400, 100, 600]

fdd995b13df1d77a5ce45bbf6069b300a2459bd4
unknown <coollucky2608@gmail.com>
2025-03-04 10:41:09 +0530

    scope_02 JS

---
+++ 16_scope_fun.js
@@ -0,0 +1,38 @@
+function one(){
+    const username = "Rahwar"
+
+    function two(){
+        const website = "Youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+    two()    
+}
+
+// one()
+
+if (true) {
+    const username = "Rahwar"
+    if (username === "Rahwar") {
+        const website = " Youtube"
+        // console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+}
+// console.log(username);
+
+//  ++++++++++++ interesting +++++++++++++++++
+
+console.log(addone(5))
+function addone(num){
+    return num + 1
+}
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}

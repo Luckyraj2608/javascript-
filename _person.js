6e0b96c48f8df5277f788eb9b1cda8c389412ea9
unknown <coollucky2608@gmail.com>
2025-03-11 23:04:57 +0530

    _comparison in JS

---
+++ _person.js
@@ -0,0 +1,9 @@
+const person = {
+    name : "Lucky"
+    greet : function() {
+        console.log(this.name);
+        
+    }
+};
+    const greet = person.greet;
+    // 
--- comparison.js
+++ comparison.js
@@ -1,6 +1,6 @@
-// console.log(2 > 1);
-// console.log(2 >= 1);
-// console.log(2 < 1);
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
 
 // console.log("2" > 1);
 // console.log("02" > 1);
@@ -16,6 +16,8 @@ console.log(undefined < 0);
 // === strict check hpta hai values ko strictly check karta hai
 
 console.log("2" === 2);
+
+
 
 
 

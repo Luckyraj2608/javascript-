92a6bbd14bfc7f1120a8cbe01193f9efe60096b6
unknown <coollucky2608@gmail.com>
2025-03-13 15:04:02 +0530

    _IIFE in JS

---
+++ _IIFE.js
@@ -0,0 +1,15 @@
+// immediately Invoked Function Expression (IIFE)
+// global scope k problem hote hai kaye baar-- global problem ko hatana k liye IIFE ka use krte hai
+//  dp IIFE k liye (;) ka use kiya jaata hai
+(function chai()
+{
+    console.log(`DB CONNECTED`);
+    
+})();
+
+
+( (name) => {
+console.log(`DB CONNECTED TWO ${name}`);
+
+})("Rahwar")
+

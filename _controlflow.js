d86d60fa034502ea274626e63d6407d34253df5d
unknown <coollucky2608@gmail.com>
2025-03-14 10:23:52 +0530

    _if_else in JS

---
+++ _controlflow.js
@@ -0,0 +1,67 @@
+// flow
+
+// const isUserloggedIn = true
+const temperature = 55
+
+if (temperature === 41) {
+console.log("less than 50");
+
+} else{
+    console.log("temperature is greater than 50");
+}
+
+console.log("executed");
+
+
+const score = 200
+
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`user power : ${power}`);
+    
+// }
+// console.log(`user power : ${power}`);
+
+
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+
+// const balance = 1000
+// // semicolon (;) k jagah pe  
+// if (balance > 500) console.log("test"),console.log("test2");
+
+
+// Nesting
+
+// if (balance < 500) {
+//     console.log("less than");
+    
+// }else if (balance < 750) {
+//     console.log("less than 750");
+    
+// }else if (balance < 900) {
+//     console.log("less than 900");
+    
+// }else {
+//     console.log("less than 1200");
+    
+// }
+
+
+const isUserloggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+
+if (isUserloggedIn && debitCard && 2==2) {
+    console.log("allow to buy couse");
+    
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("user logged in");
+    
+}
+

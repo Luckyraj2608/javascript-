3a32abe9188b817141c4183dcbed7f6d8643b34f
unknown <coollucky2608@gmail.com>
2025-03-17 21:54:19 +0530

    _json in jS

---
+++ _JSON_02.js
@@ -0,0 +1,11 @@
+// JSON data
+const person = {
+    "name": "John",
+    "age": 30,
+    "city": "New York"
+};
+
+// Accessing JSON data
+console.log(person.name); // Output: John
+console.log(person.age);  // Output: 30
+console.log(person.city); // Output: New York
--- _student.js
+++
@@ -1,9 +0,0 @@
-const { exec } = require('child_process');
-
-exec('git add . && git commit -m "Initial commit" && git push origin main', (err, stdout, stderr) => {
-    if (err) {
-        console.error(`Error: ${stderr}`);
-        return;
-    }
-    console.log(stdout);
-});

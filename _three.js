0200ecf296a8b5c9731beeed161a5df65e0521d1
unknown <coollucky2608@gmail.com>
2025-03-17 22:23:00 +0530

    _for_of in JS

---
+++ _three.js
@@ -0,0 +1,40 @@
+// for of
+
+["", "", ""]
+[{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    // console.log(num);
+    
+}
+
+const greeting = "Hello world"
+for (const greet of greeting) {
+    // console.log(`Each character is ${greet}`);
+    
+}
+
+// Maps  value unique rhta hai
+
+const map = new Map()
+map.set('IN', "INDIA")
+map.set('USA', "UNITED STATES OF AMERICA")
+map.set('Fr', "France")
+// console.log(map);
+
+for (const [key, value] of map) {
+    console.log(key, ':-', value);
+    
+}
+
+const myObject = {
+    'game1': 'NFS',
+    'game2': 'Spiderman',
+}
+
+for (const [key, value] of myObject) {
+    console.log(key, ':-', value);
+    
+}

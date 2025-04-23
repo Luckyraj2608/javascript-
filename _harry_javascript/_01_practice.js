c482833209e9b7f7690eba3c6a4d879e95bc13a7
unknown <coollucky2608@gmail.com>
2025-04-23 19:11:50 +0530

    basic_JS

---
+++ _harry_javascript/_01_practice.js
@@ -0,0 +1,19 @@
+// chapter 1 -= Q1
+// let a = "harry"
+// let b = 6
+// console.log(a + b);
+
+// chapter 1 -> Q2
+// console.log(typeof (a + b));
+
+const a = {
+    name: "harry",
+    section:1,
+    isPrincipal: false
+
+}
+a['friend'] = "lucky"
+a["name"] = "nanhi"
+
+console.log(a);
+
---
+++ _harry_javascript/_01_variable.js
@@ -0,0 +1,28 @@
+// console.log("this is javascript code");
+// let a = 67
+// console.log(a);
+// a = "lucky"
+// console.log(a);
+
+
+// var a = 45;
+// var b = "lucky";
+// var c = null
+// var d = undefined
+
+// console.log(b);
+
+// nn bb ss u
+let a = null;
+let b = 345;
+let c = true; // can also be false
+let d = BigInt("567")
+let e = "Harry"
+// let f = symbol("I am a nice symbol")
+let g = undefined
+
+console.log(a, b, c, d, e, g);
+console.log(typeof d);
+
+
+

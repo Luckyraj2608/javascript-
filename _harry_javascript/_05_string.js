8978b69576d72761409cfcc8527b5b9fecc5e46e
unknown <coollucky2608@gmail.com>
2025-04-24 16:02:15 +0530

    _array in JS

---
+++ _harry_javascript/_05_string.js
@@ -0,0 +1,6 @@
+let name = "Lucky\""
+console.log(name.length);
+
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+console.log(name.slice(2, 4));
---
+++ _harry_javascript/_06_array.js
@@ -0,0 +1,15 @@
+let marks_class_12 = [91, 82, 73, 64, false, "not present"]
+console.log(marks_class_12)
+console.log(marks_class_12[0])
+console.log(marks_class_12[1])
+console.log(marks_class_12[2])
+console.log(marks_class_12[3])
+console.log(marks_class_12[4])
+console.log(marks_class_12[5])
+console.log(marks_class_12[6])
+console.log("The length of marks_class_12", marks_class_12.length)
+marks_class_12[6] = 89
+marks_class_12[0] = 96
+console.log(marks_class_12);
+
+
